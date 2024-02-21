c15093a5dc3754a7f5132762fcf8134842ed9104
Breck Houghton <breck.houghton@gmail.com>
2024-02-21 10:56:46 -0800

    Revert "Prep for Vercel"
    This reverts commit 297100f8d1b2436927dcc9178e176cd979f176c5.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
-
+import { Home } from "./pages/Home";
 import { Store } from "./pages/Store";
-
+import { About } from "./pages/About";
 import { Navbar } from "./components/Navbar";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
@@ -12,7 +12,18 @@ function App() {
       <Navbar />
       <Container className="mb-4">
         <Routes>
-          <Route path="/store" element={<Store />} />
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/store"
+            element={<Store />}
+          />
+          <Route
+            path="/about"
+            element={<About />}
+          />
         </Routes>
       </Container>
     </ShoppingCartProvider>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,11 +5,25 @@ import { useShoppingCart } from "../context/ShoppingCartContext";
 export function Navbar() {
   const { openCart, cartQuantity } = useShoppingCart();
   return (
-    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+    <NavbarBs
+      sticky="top"
+      className="bg-white shadow-sm mb-3">
       <Container>
         <Nav className="me-auto">
-          <Nav.Link to={"/store"} as={NavLink}>
+          <Nav.Link
+            to={"/"}
+            as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link
+            to={"/store"}
+            as={NavLink}>
             Store
+          </Nav.Link>
+          <Nav.Link
+            to={"/about"}
+            as={NavLink}>
+            About
           </Nav.Link>
         </Nav>
         {cartQuantity > 0 && (
@@ -17,8 +31,7 @@ export function Navbar() {
             onClick={openCart}
             style={{ width: "3rem", height: "3rem", position: "relative" }}
             variant="outline-primary"
-            className="rounded-circle"
-          >
+            className="rounded-circle">
             <i className="material-icons">shopping_cart</i>
 
             <div
@@ -31,8 +44,7 @@ export function Navbar() {
                 bottom: 0,
                 right: 0,
                 transform: "translate(25%, 25%)",
-              }}
-            >
+              }}>
               {cartQuantity}
             </div>
           </Button>
