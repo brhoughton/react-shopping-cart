297100f8d1b2436927dcc9178e176cd979f176c5
Breck Houghton <breck.houghton@gmail.com>
2024-02-21 10:47:34 -0800

    Prep for Vercel

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,25 +5,11 @@ import { useShoppingCart } from "../context/ShoppingCartContext";
 export function Navbar() {
   const { openCart, cartQuantity } = useShoppingCart();
   return (
-    <NavbarBs
-      sticky="top"
-      className="bg-white shadow-sm mb-3">
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
         <Nav className="me-auto">
-          <Nav.Link
-            to={"/"}
-            as={NavLink}>
-            Home
-          </Nav.Link>
-          <Nav.Link
-            to={"/store"}
-            as={NavLink}>
+          <Nav.Link to={"/store"} as={NavLink}>
             Store
-          </Nav.Link>
-          <Nav.Link
-            to={"/about"}
-            as={NavLink}>
-            About
           </Nav.Link>
         </Nav>
         {cartQuantity > 0 && (
@@ -31,7 +17,8 @@ export function Navbar() {
             onClick={openCart}
             style={{ width: "3rem", height: "3rem", position: "relative" }}
             variant="outline-primary"
-            className="rounded-circle">
+            className="rounded-circle"
+          >
             <i className="material-icons">shopping_cart</i>
 
             <div
@@ -44,7 +31,8 @@ export function Navbar() {
                 bottom: 0,
                 right: 0,
                 transform: "translate(25%, 25%)",
-              }}>
+              }}
+            >
               {cartQuantity}
             </div>
           </Button>
